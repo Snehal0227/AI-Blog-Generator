64797ce0ae788ba940a577a8cba0058ef76a1b60
Snehal <snehaldarade12@gmail.com>
2026-08-11 06:09:14 -0700

    Prepare Project for deployment

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+
 class Blog(db.Model):
 
     __tablename__ = "blogs"
@@ -23,8 +24,17 @@ class Blog(db.Model):
 
     ai_image = db.Column(db.String(255))
 
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    created_at = db.Column(
+        db.DateTime,
+        server_default=db.func.now()
+    )
 
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.id")
+    )
 
-    views = db.Column(db.Integer, default=0)
+    views = db.Column(
+        db.Integer,
+        default=0
+    )
